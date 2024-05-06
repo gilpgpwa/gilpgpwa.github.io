c9fca039c2b9c332357635f35d53b25dad805449
gilpgdm <gilpgdm@gmail.com>
2024-05-06 08:34:09 -0600

    sw 2.1

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * conoce como número menor y se cambia cuando se realizan
  * modificaciones menores.
  */
-const VERSION = "2.00"
+const VERSION = "2.1"
 
 /** Nombre del archivo de cache. */
 const CACHE = "ejemploPWA"
